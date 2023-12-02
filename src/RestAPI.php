d9e02535699a6ef74ae50d443fa8d0f829502a77
Latif Harkat <109482864+Latip176@users.noreply.github.com>
2023-12-02 22:11:28 +0700

    Add files via upload

--- src/RestAPI.php
+++ src/RestAPI.php
@@ -1,1 +1,47 @@
+<?php
 
+namespace Request\Latip176;
+
+class Data {
+    public $data;
+    public $action;
+    public $urlapi;
+    
+    public function __construct($data = null) {
+        $this->data = $data;
+        $this->urlapi = "https://latipharkat-api.my.id/api/otakudesu";
+    }
+}
+
+class RestAPI extends Data {
+    public function getData($next) {
+        $url = $this->urlapi . $next;
+        $req = curl_init();
+        curl_setopt($req, CURLOPT_URL, $url);
+        curl_setopt($req, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($req);
+        curl_close($req);
+ 
+        return $output;
+    }
+    
+    public function request($action) {
+        switch($action) {
+            case "home":
+                $this->action = "/home";
+                break;
+            case "view":
+                $this->action = "/view/?data={$this->data}";
+                break;
+            case "info":
+                $this->action = "/info/?data={$this->data}";
+                break;
+            case "search":
+                $this->action = "/search/?keyword={$this->data}";
+                break;
+        }
+        return $this->getData($this->action);
+    }
+}
+
+?>
