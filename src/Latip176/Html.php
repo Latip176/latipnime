82574a56347fb81af6d74666588b4f766852d7bc
Latif Harkat <109482864+Latip176@users.noreply.github.com>
2023-12-03 22:47:21 +0700

    Add files via upload

---
+++ src/Latip176/Html.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Latip176;
+
+class Html {
+    public static function navbar() {
+        return <<<EOD
+            <div class="navbar">
+                <div class="nav">
+                    <ul class="list">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">Ongoing</a></li>
+                        <li><a href="https://wa.me/6283870396203">Contact Me</a></li>
+                        <li>
+                            <form action="/search" method="get">
+                                <input type="text" name="keyword" class="keyword" placeholder="cari anime favorite kamu"> <button type="submit">Search</button>
+                            </form>
+                        </li>
+                    </ul>
+                    <ul class="open">
+                        <li>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </li>
+                    </ul>
+                    <ul class="close">
+                        <li>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </li>
+                    </ul>
+                    <span class="nav-brand" style="font-size: 20px;"><span style="color: #00f; text-shadow: 0 0 10px #00f, 0 0 20px #00f, 0 0 30px #00f;">Latip</span>Nime</span>
+                </div>
+            </div>
+            <div class="navbar-content">
+                <ul>
+                    <li id="item"><a href="index.php">Home</a></li>
+                    <li id="item"><a href="#">Ongoing</a></li>
+                    <li id="item"><a href="https://wa.me/6283870396203">Contact Me</a></li>
+                    <li id="item">
+                        <form action="/search" method="get">
+                            <input type="text" name="keyword" class="keyword" placeholder="cari anime favorite kamu"> <button type="submit">Search</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        EOD;
+    }
+    
+    public static function footer() {
+        return <<<EOD
+            <div class="footer">
+                <div style="width: 100%; height: 100px; text-align: center; ">
+                    <p class="text-light fw-bold" style="line-height: 100px;">&copy; 2023 - 2024 Made By ❤️ <a href="https://latipharkat.my.id/" class="latip">Latip176</a></p>
+                </div>
+            </div>
+        EOD;
+    }
+}
--- src/Latip176/RestAPI.php
+++ src/Latip176/RestAPI.php
@@ -1,1 +1,49 @@
+<?php
 
+namespace Latip176;
+
+class Data {
+    public $data;
+    public $action;
+    public $urlapi;
+    
+    public function __construct($data = null) {
+        $this->data = $data;
+        $this->urlapi = "https://latipharkat-api.my.id/api/otakudesu";
+    }
+}
+
+class RestAPI extends Data {
+    private function getData($next) {
+        $url = str_replace(" ", "+", $this->urlapi . $next);
+        $req = curl_init();
+        curl_setopt($req, CURLOPT_URL, $url);
+        curl_setopt($req, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($req, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($req, CURLOPT_FOLLOWLOCATION, TRUE);
+        $output = curl_exec($req);
+        curl_close($req);
+ 
+        return $output;
+    }
+    
+    public function request($action) {
+        switch($action) {
+            case "home":
+                $this->action = "/home";
+                break;
+            case "view":
+                $this->action = "/view/?data={$this->data}";
+                break;
+            case "info":
+                $this->action = "/info/?data={$this->data}";
+                break;
+            case "search":
+                $this->action = "/search/?keyword={$this->data}";
+                break;
+        }
+        return $this->getData($this->action);
+    }
+}
+
+?>
