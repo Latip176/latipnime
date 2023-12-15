7f662c61ae071c002dd8ae53ffd6294d4ab24865
Latif Harkat <109482864+Latip176@users.noreply.github.com>
2023-12-15 20:43:38 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
-require __DIR__ . '/../index.php';
-require __DIR__ . '/../anime/index.php';
+$requestPath = $_SERVER['REQUEST_URI'];
+
+if (strpos($requestPath, "/anime/") !== false) {
+    require __DIR__ . '/../anime/index.php';
+} else if (strpos($requestPath, "/") !== false) {
+    require __DIR__ . '/../index.php';
+}
